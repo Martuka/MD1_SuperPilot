02019618f9b49252cf9389c4d6b100581a9d7dbd
Laurent Weingart <l.weingart@me.com>
2016-10-22 20:55:49 +0200

    update code to only use new articles

--- rss_feeds/guardian_rss.py
+++ rss_feeds/guardian_rss.py
@@ -5,16 +5,26 @@
 # Basic rss feeds analyser for The Guardian RSS feed
 # This program needs python 3 to run
 
+###############################################################################
+## Imports
+
 import feedparser
 import requests
 import datetime
+import time
+import sys
 import re as regex
 from lxml import html
 from collections import Counter
 from collections import OrderedDict
 from operator import itemgetter
 
-DEBUG = False
+
+###############################################################################
+## Global Variables
+
+DEBUG = True
+WRITE_LOGS = True
 article_counter = 0
 global_match_occurrences = 0
 
@@ -27,19 +37,25 @@ key_words_list = "keywords.txt"
 article_class_name = "content__article-body"
 log_file = "results.txt"
 
+
 ###############################################################################
 ## Functions
 
-# parse the feeds urls file and put each element in a list
+# Print usage of this program.
+def print_usage():
+	print("usage: guardian_rss.py <interval_time>")
+	print("interval_time = time in seconds between two checks for new RSS feeds.")
+
+# Parse the feeds urls file and put each element in a list
 def list_feeds_urls(feeds_file):
 	feeds_list = [line.strip() for line in open(feeds_file, 'r')]
 	return feeds_list
 
-# parse the tags file and put each elements in a list
+# Parse the tags file and put each elements in a list
 def get_key_words(keywords_file):
 	return set(list_feeds_urls(keywords_file))
 
-# put content in the file filename and writes the file in the file system
+# Put content in the file filename and writes the file in the file system
 def write_file(filename, content):
 	with open(filename, "w") as file_to_write:
 		file_to_write.write(content)
@@ -50,7 +66,7 @@ def create_word_list_from_text(text):
 	foo = regex.sub('[^a-z\ \']+', " ", text.lower())
 	return list(foo.split())
 
-# extract the text from an url and the class name of a specific div.
+# Extract the text from an url and the class name of a specific div.
 def get_article_from_rss_url(url, div_class_name):
 	response = requests.get(url)
 	tree = html.fromstring(response.content)
@@ -60,82 +76,100 @@ def get_article_from_rss_url(url, div_class_name):
 	else:
 		return ""
 
+#
 def my_print(string):
-	print(string)
-	logs.write(string)
+	if DEBUG:
+		print(string)
+	if WRITE_LOGS:
+		logs.write(string + "\n")
+
+# Transform a datetime.datetime in its number of seconds from epoch
+def time_in_seconds_from_date(dt):
+	epoch = datetime.datetime.fromtimestamp(0)
+	return (dt - epoch).total_seconds()
+
+# Transform a time.stuct_time object into a datetime.datetime object
+def date_from_time_struct(t):
+	return datetime.datetime.fromtimestamp(time.mktime(t))
+
+# Create a datetime.datetime from a numbre of seconds from epoch
+def date_from_seconds(seconds):
+	return datetime.datetime.fromtimestamp(seconds)
+
+# Verify if an article has been published more recently that interval time.
+def new_article(entry, now, interval):
+	published_parsed = entry.published_parsed
+	delta = time_in_seconds_from_date(now) - time_in_seconds_from_date(date_from_time_struct(published_parsed))
+	if delta <= interval:
+		return True
+	return False
+
+
 ###############################################################################
 ## Main
 if __name__ == '__main__':
+
+	if len(sys.argv) != 2:
+		print_usage()
+		sys.exit(0)
+
+	try:
+		interval_time = int(sys.argv[1])
+	except ValueError as ex:
+		print('"%s" does not represent a number of seconds: %s' % (sys.argv[1], ex))
+		print_usage()
+		sys.exit(0)
+
+	now = datetime.datetime.now()
 
 	logs = open(log_file, "w")
 
 	hit_list = list_feeds_urls(rss_urls_list)
 	key_words =	get_key_words(key_words_list)
-	print("\n")
-	print("\n")
-	print("====================================================================")
-	print("=========================                  =========================")
-	print("========================= RSS The Guardian =========================")
-	logs.write("====================================================================\n")
-	logs.write("=========================                  =========================\n")
-	logs.write("========================= RSS The Guardian =========================\n")
-	# print("\nRSS urls to check = ", hit_list)
-	print("\n")
-	print("Key words = ", key_words)
-	print("\n")
-	logs.write("\n")
-	logs.write("Key words =\n")
-	logs.write(" ".join(key_words))
+	my_print("\n")
+	my_print("\n")
+	my_print("====================================================================")
+	my_print("=========================                  =========================")
+	my_print("========================= RSS The Guardian =========================")
+	my_print("\n")
+	my_print("Key words = \n")
+	my_print(" ".join(key_words))
 
 	matching_words = list()
 	for rss_url in hit_list:
-		print("************* ************* ************* *************")
-		print("RSS url:", rss_url)
-		logs.write("\n\n************* ************* ************* *************\n")
-		logs.write("RSS url: {}".format(rss_url))
+		my_print("\n\n************* ************* ************* *************\n")
+		my_print("RSS url: {}".format(rss_url))
 		feeds = feedparser.parse(rss_url)
 		entries = feeds.entries
 
 		for entry in entries:
-			print("************* ************* ************* *************")
-			logs.write("\n************* ************* ************* *************\n\n")
-			article_list = list()
-			web_page_url = entry.link
-			article = get_article_from_rss_url(web_page_url, article_class_name)
-			wordlist = create_word_list_from_text(article)
-			print("\n")
-			print("Word list from article:")
-			print(wordlist)
-			logs.write("\nArticle:  '{}'\n\n".format(entry.title))
-			logs.write("Word list:\n")
-			logs.write(", ".join(wordlist))
+			if new_article(entry, now, interval_time):
+				my_print("\n************* ************* ************* *************\n\n")
+				article_list = list()
+				web_page_url = entry.link
+				article = get_article_from_rss_url(web_page_url, article_class_name)
+				wordlist = create_word_list_from_text(article)
+				my_print("\nArticle:  '{}'\n\n".format(entry.title))
+				my_print("Word list:\n")
+				my_print(", ".join(wordlist))
 
-			for word in wordlist:
-				if word in key_words:
-					article_list.append(word)
+				for word in wordlist:
+					if word in key_words:
+						article_list.append(word)
 
-			matching_words.extend(article_list)
-			match_occurrences = len(article_list)
-			print("\n")
-			print("\nThere were {} words from the key words list in this article".format(match_occurrences))
-			print("\n")
-			print("Those words were:")
-			print(article_list)
-			print("\n")
-			logs.write("\n\nThere were {} words from the key words list in this article\n".format(match_occurrences))
-			logs.write("Those words were:\n")
-			logs.write(", ".join(article_list))
-			logs.write("\n")
-			global_match_occurrences += match_occurrences
+				matching_words.extend(article_list)
+				match_occurrences = len(article_list)
+				my_print("\n\nThere were {} words from the key words list in this article\n".format(match_occurrences))
+				my_print("Those words were:\n")
+				my_print(", ".join(article_list))
+				my_print("\n")
+				global_match_occurrences += match_occurrences
 
-	print("========================= Final result =========================")
-	print("In total, there were {} word matches.".format(global_match_occurrences))
-	print("\n")
+
 	result = Counter(matching_words)
-	logs.write("\n\n========================= Final result =========================\n")
-	logs.write("In total, there were {} word matches.\n".format(global_match_occurrences))
+	my_print("\n\n========================= Final result =========================\n")
+	my_print("In total, there were {} word matches.\n".format(global_match_occurrences))
 	for k, v in sorted(result.items(), key=itemgetter(1), reverse=True):
-		print(k, v)
-		logs.write("{}: {}\n".format(k, v))
+		my_print("{}: {}\n".format(k, v))
 
 	logs.close()
