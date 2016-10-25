5572bdbbb08b7c62899d4877994b541dae8928a5
Martuka <marta@loccomotion.ch>
2016-10-25 14:19:48 +0000

    update pilots.sh

--- rss_feeds/pilot.sh
+++ rss_feeds/pilot.sh
@@ -10,4 +10,4 @@ fi
 crontab -u pi -l | grep -v 'guardian_rss_motor.py'  | crontab -u pi -
 
 # add the new entry
-(crontab -u pi -l ; echo "*/$1 * * * * pi /usr/bin/python /home/pi/git/MD1_SuperPilot/rss_feeds/guardian_rss_motor.py $1*60") | crontab -u pi -
+(crontab -u pi -l ; echo "*/$1 * * * * pi /usr/bin/python /home/pi/git/MD1_SuperPilot/rss_feeds/guardian_rss_motor.py $1") | crontab -u pi -
