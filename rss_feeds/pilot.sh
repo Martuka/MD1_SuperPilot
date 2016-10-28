bf0ccb072c7d9e8e86bf77c9a4c50ddf456c299e
Laurent Weingart <l.weingart@me.com>
2016-10-28 20:13:00 +0200

    modify pilot to use hardcoded value

--- rss_feeds/pilot.sh
+++ rss_feeds/pilot.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-	#statements
-	echo '[#] usage: ' $0 '<nb_minutes>'
-	exit 0
-fi
-
 # remove old entry
 crontab -u pi -l | grep -v 'guardian_rss_motor.py'  | crontab -u pi -
 
 # add the new entry
-(crontab -u pi -l ; echo "*/$1 * * * * pi /usr/bin/python /home/pi/git/MD1_SuperPilot/rss_feeds/guardian_rss_motor.py $1") | crontab -u pi -
+(crontab -u pi -l ; echo "*/1 * * * * pi /usr/bin/python /home/pi/git/MD1_SuperPilot/rss_feeds/guardian_rss_motor.py") | crontab -u pi -
